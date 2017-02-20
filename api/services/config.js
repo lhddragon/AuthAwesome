d8a28731a4b0c375fb1ad6565eb0e960d0dcb193
Hongda Li <lhddragon87@gmail.com>
2017-02-20 17:22:22 -0500

    override all with working version

--- api/services/config.js
+++ api/services/config.js
@@ -2,6 +2,6 @@ module.exports = {
 	FACEBOOK_SECRET: '7e8d2d62f57cc692024d504cabc0141d',
 	GOOGLE_SECRET: 'FXBFCa82cD6i9v3dgrIDM-wY',
 	EMAIL_SECRET: 'something',
-	SMTP_PASS: 'Gistic14!',
+	SMTP_PASS: 'emailpassword',
 	APP_URL: 'http://localhost:9003'
 }
