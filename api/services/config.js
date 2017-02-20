b296ab96bab636c84a55d5eac3b3eb846f985e06
Hongda Li <lhddragon87@gmail.com>
2017-02-20 17:19:14 -0500

    Revert "update api"
    This reverts commit 2006a21b4b1626790ce1e1fde73f188f55bdcfa0.

--- api/services/config.js
+++ api/services/config.js
@@ -2,6 +2,12 @@ module.exports = {
 	FACEBOOK_SECRET: '7e8d2d62f57cc692024d504cabc0141d',
 	GOOGLE_SECRET: 'FXBFCa82cD6i9v3dgrIDM-wY',
 	EMAIL_SECRET: 'something',
+<<<<<<< HEAD
 	SMTP_PASS: 'emailpassword',
 	APP_URL: 'http://localhost:9003'
 }
+=======
+	SMTP_PASS: 'universe',
+	APP_URL: 'http://localhost:9000'
+}
+>>>>>>> parent of 2006a21... update api
